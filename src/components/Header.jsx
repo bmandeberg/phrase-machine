c4014c7d0f1ac97eee0dd3cae2ff2919d15f1f66
Ben Mandeberg <bmandeberg@ucla.edu>
2022-06-13 01:17:03 -0700

    New logo images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 import './Header.scss'
 
 export default function Header() {
-  return <div id="header">
-    <img src={logo} alt="" id="logo" />
-  </div>
+  return (
+    <div id="header">
+      <img src={logo} alt="" id="logo" />
+    </div>
+  )
 }
