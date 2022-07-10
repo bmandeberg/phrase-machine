6a43b8a7b388eafa606a1ded71c173fd2ad7760d
Ben Mandeberg <bmandeberg@ucla.edu>
2022-07-10 03:17:44 -0700

    Delimiter position fixed

--- src/components/Delimiter.jsx
+++ src/components/Delimiter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { useHover } from 'react-use-gesture'
@@ -6,6 +6,7 @@ import { timeToPixels } from '../util'
 import delimiterGraphic from '../assets/delimiter.svg'
 import xIcon from '../assets/x-icon-purple.svg'
 import './Delimiter.scss'
+import { KEYS_WIDTH } from '../globals'
 
 export default function Delimiter({ delimiter, i, deleteDelimiter, dragging, wasDragging, dragHover }) {
   const [active, setActive] = useState(dragging || wasDragging.current === i)
@@ -40,14 +41,13 @@ export default function Delimiter({ delimiter, i, deleteDelimiter, dragging, was
     setHovering(e.hovering)
   })
 
+  const left = useMemo(
+    () => (delimiter.snap ? timeToPixels({ [delimiter.snap]: delimiter.snapNumber }) : delimiter.x) + 34 + KEYS_WIDTH,
+    [delimiter.snap, delimiter.snapNumber, delimiter.x]
+  )
+
   return (
-    <div
-      className={classNames('delimiter', { active })}
-      index={i}
-      {...hover()}
-      style={{
-        left: delimiter.snap ? timeToPixels({ [delimiter.snap]: delimiter.snapNumber }) : delimiter.x,
-      }}>
+    <div className={classNames('delimiter', { active })} index={i} {...hover()} style={{ left }}>
       <img className="delimiter-head" src={delimiterGraphic} alt="" draggable="false" />
       <div className="delimiter-grab"></div>
       <img className="delimiter-x" src={xIcon} alt="" draggable="false" onClick={() => deleteDelimiter(i)} />
