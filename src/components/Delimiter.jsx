9c0d4510ce247150b138bd0facad1627d26d9991
Ben Mandeberg <bmandeberg@ucla.edu>
2022-08-01 00:49:53 -0700

    Cleanup

--- src/components/Delimiter.jsx
+++ src/components/Delimiter.jsx
@@ -53,16 +53,11 @@ export default function Delimiter({ delimiter, i, deleteDelimiter, dragging, was
       style={{ left, '--delimiter-height': height + 'px' }}>
       <img className="delimiter-head" src={delimiterGraphic} alt="" draggable="false" />
       <div className="delimiter-grab"></div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="humbleicons hi-times delimiter-x">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="humbleicons hi-times delimiter-x">
         <g xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-width="2">
           <path d="M6 18L18 6M18 18L6 6" />
         </g>
       </svg>
-      {/* <img className="delimiter-x" src={xIcon} alt="" draggable="false" onClick={() => deleteDelimiter(i)} /> */}
     </div>
   )
 }
